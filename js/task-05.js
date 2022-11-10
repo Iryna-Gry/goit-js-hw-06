f23e2423c2e7368ccbaad2c48642925bc325b65a
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2022-11-10 16:49:58 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,10 @@
 const inputEl = document.querySelector("input#name-input");
 const spanNameOutput = document.querySelector("span#name-output");
-const replaceNameOutput = (event) => {
+const onNameInput = (event) => {
   if (event.currentTarget.value === "") {
     spanNameOutput.textContent = "Anonymous";
   } else {
     spanNameOutput.textContent = event.currentTarget.value;
   }
 };
-inputEl.addEventListener("input", replaceNameOutput);
+inputEl.addEventListener("input", onNameInput);
