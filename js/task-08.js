410b2595b43a4afefd88d382ebca03f20fc6ef89
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2022-11-11 12:04:29 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector("form.login-form");
+form.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(event.currentTarget);
+  if (form.email === "" || form.password === "") {
+    return alert("Все поля должны быть заполнены");
+  } else {
+    const userData = ({
+      elements: { email, password },
+    } = event.currentTarget);
+  }
+  console.log(userData);
+  event.currentTarget.reset();
+}
